758f33e00320e6104edefabf3a1485f22ccac4ca
신동하 <sinwindis@naver.com>
2018-05-24 18:37:16 +0900

    RecipeSearch, Ingredient 메소드 수정

--- app/src/main/java/com/homechef/ict/ict_homechef/Ingredient.java
+++ app/src/main/java/com/homechef/ict/ict_homechef/Ingredient.java
@@ -1,15 +1,22 @@
 package com.homechef.ict.ict_homechef;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 public class Ingredient {
 
     private String name;
     private int search_mode = 0;
 
-    public Ingredient(String s)
+    Ingredient(String s)
     {
         name = s;
     }
-    public Ingredient(String s, int mode)
+    Ingredient(String s, int mode)
     {
         name = s;
         search_mode = mode;
@@ -24,7 +31,7 @@ public class Ingredient {
         return search_mode;
     }
 
-    public void cycleSearchMode()
+    private void cycleSearchMode()
     {
         if(search_mode == 2)
         {
@@ -35,4 +42,56 @@ public class Ingredient {
             search_mode++;
         }
     }
+
+    public void setName(String s)
+    {
+        name = s;
+    }
+
+    public void addIngredientView(LinearLayout ll, Context viewClass)
+    {
+
+        //동적 textview 생성
+        final TextView textview_ingredient = new TextView(viewClass);
+        textview_ingredient.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        textview_ingredient.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+
+        textview_ingredient.setTextColor(Color.parseColor("#000000"));
+        textview_ingredient.setTextSize(16);
+        textview_ingredient.setText(Ingredient.this.getName());
+        textview_ingredient.setSingleLine();
+        ll.addView(textview_ingredient);
+
+        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_green);
+
+
+        //버튼화
+        textview_ingredient.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Ingredient.this.cycleSearchMode();
+                switch (Ingredient.this.getSearchMode())
+                {
+                    case 0:
+                        textview_ingredient.setTextColor(Color.parseColor("#000000"));
+                        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_green);
+                        break;
+                    case 1:
+                        textview_ingredient.setTextColor(Color.parseColor("#000000"));
+                        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_blue);
+                        break;
+                    case 2:
+                        textview_ingredient.setTextColor(Color.parseColor("#000000"));
+                        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_red);
+                        break;
+                }
+            }
+        });
+
+
+    }
 }
+
+
--- app/src/main/java/com/homechef/ict/ict_homechef/SearchActivity.java
+++ app/src/main/java/com/homechef/ict/ict_homechef/SearchActivity.java
@@ -2,33 +2,30 @@ package com.homechef.ict.ict_homechef;
 
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 import android.os.Bundle;
 import android.widget.Toast;
 
 
-public class SearchActivity extends AppCompatActivity
-{
+public class SearchActivity extends AppCompatActivity {
     final int MAXINGREDIENT = 10;
     final int MAXLAYOUTSIZE = 20;
     final int MARGINSIZE = 2;
     int ingredientNum = 0;
-    int [] lineSize = { 0, 0, 0, 0 };
+    int[] lineSize = {0, 0, 0, 0};
     Ingredient[] ingredientSet = new Ingredient[MAXINGREDIENT];
+    String temp;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-
 
 
         final LinearLayout llNowIngredient1 = findViewById(R.id.ll_nowingredient1);
@@ -40,13 +37,18 @@ public class SearchActivity extends AppCompatActivity
         Button btnAddIngredient = findViewById(R.id.btn_ingredientadd);
         Button btnSearch = findViewById(R.id.btn_search);
 
+        for(int i = 0; i < MAXINGREDIENT; i++)
+        {
+            ingredientSet[i] = new Ingredient(null, 0);
+        }
+
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //RecipeListActivity에 데이터 전달
                 Intent intent = new Intent(SearchActivity.this, RecipeListActivity.class);
-                for(int i = 0; i < ingredientNum; i++)
-                {
+                for (int i = 0; i < ingredientNum; i++) {
                     String[] data = new String[2];
                     data[0] = ingredientSet[i].getName();
                     data[1] = String.valueOf(ingredientSet[i].getSearchMode());
@@ -60,34 +62,33 @@ public class SearchActivity extends AppCompatActivity
         btnAddIngredient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( edtAddIngredient.getText().toString().length() > 0 ) {
+                temp = edtAddIngredient.getText().toString();
+                edtAddIngredient.setText(null);
+                if (temp.length() > 0) {
 
-                    if(lineSize[0] <= MAXLAYOUTSIZE)
+                    if(addIngredient(temp, ingredientNum))
                     {
-                        addIngredient(edtAddIngredient.getText().toString(), llNowIngredient1);
-                        lineSize[0] += edtAddIngredient.getText().toString().length();
-                        lineSize[0] += MARGINSIZE;
-                    }
-                    else if(lineSize[1] <= MAXLAYOUTSIZE)
-                    {
-                        addIngredient(edtAddIngredient.getText().toString(), llNowIngredient2);
-                        lineSize[1] += edtAddIngredient.getText().toString().length();
-                        lineSize[1] += MARGINSIZE;
-                    }
-                    else if(lineSize[2] <= MAXLAYOUTSIZE)
-                    {
-                        addIngredient(edtAddIngredient.getText().toString(), llNowIngredient3);
-                        lineSize[2] += edtAddIngredient.getText().toString().length();
-                        lineSize[2] += MARGINSIZE;
-                    }
-                    else if(lineSize[3] <= MAXLAYOUTSIZE)
-                    {
-                        addIngredient(edtAddIngredient.getText().toString(), llNowIngredient4);
-                        lineSize[3] += edtAddIngredient.getText().toString().length();
-                        lineSize[3] += MARGINSIZE;
+                        if (lineSize[0] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient1, SearchActivity.this);
+                            lineSize[0] += temp.length();
+                            lineSize[0] += MARGINSIZE;
+                        } else if (lineSize[1] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient2, SearchActivity.this);
+                            lineSize[1] += temp.length();
+                            lineSize[1] += MARGINSIZE;
+                        } else if (lineSize[2] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient3, SearchActivity.this);
+                            lineSize[2] += temp.length();
+                            lineSize[2] += MARGINSIZE;
+                        } else if (lineSize[3] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient4, SearchActivity.this);
+                            lineSize[3] += temp.length();
+                            lineSize[3] += MARGINSIZE;
+                        }
+
+                        ingredientNum++;
                     }
 
-                    edtAddIngredient.setText(null);
 
                 }
             }
@@ -98,34 +99,29 @@ public class SearchActivity extends AppCompatActivity
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 //Enter key Action
                 if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    if ( edtAddIngredient.getText().toString().length() > 0 ) {
-
-                        if(lineSize[0] <= MAXLAYOUTSIZE)
-                        {
-                            addIngredient(edtAddIngredient.getText().toString(), llNowIngredient1);
-                            lineSize[0] += edtAddIngredient.getText().toString().length();
+                    temp = edtAddIngredient.getText().toString();
+                    edtAddIngredient.setText(null);
+                    if(addIngredient(temp, ingredientNum))
+                    {
+                        if (lineSize[0] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient1, SearchActivity.this);
+                            lineSize[0] += temp.length();
                             lineSize[0] += MARGINSIZE;
-                        }
-                        else if(lineSize[1] <= MAXLAYOUTSIZE)
-                        {
-                            addIngredient(edtAddIngredient.getText().toString(), llNowIngredient2);
-                            lineSize[1] += edtAddIngredient.getText().toString().length();
+                        } else if (lineSize[1] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient2, SearchActivity.this);
+                            lineSize[1] += temp.length();
                             lineSize[1] += MARGINSIZE;
-                        }
-                        else if(lineSize[2] <= MAXLAYOUTSIZE)
-                        {
-                            addIngredient(edtAddIngredient.getText().toString(), llNowIngredient3);
-                            lineSize[2] += edtAddIngredient.getText().toString().length();
+                        } else if (lineSize[2] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient3, SearchActivity.this);
+                            lineSize[2] += temp.length();
                             lineSize[2] += MARGINSIZE;
-                        }
-                        else if(lineSize[3] <= MAXLAYOUTSIZE)
-                        {
-                            addIngredient(edtAddIngredient.getText().toString(), llNowIngredient4);
-                            lineSize[3] += edtAddIngredient.getText().toString().length();
+                        } else if (lineSize[3] + temp.length() <= MAXLAYOUTSIZE) {
+                            ingredientSet[ingredientNum].addIngredientView(llNowIngredient4, SearchActivity.this);
+                            lineSize[3] += temp.length();
                             lineSize[3] += MARGINSIZE;
                         }
-                        edtAddIngredient.setText(null);
 
+                        ingredientNum++;
                     }
                     return true;
                 }
@@ -133,78 +129,22 @@ public class SearchActivity extends AppCompatActivity
             }
         });
 
+
     }
 
-
-
-
-    private void addIngredient(String s, LinearLayout ll)
-    {
-
-        //재료 최대 갯수 제한
-        if(ingredientNum == MAXINGREDIENT)
-        {
+    private boolean addIngredient(String s, int num) {
+        if (ingredientNum == MAXINGREDIENT) {
             Toast.makeText(SearchActivity.this, "더 이상 재료를 추가할 수 없습니다", Toast.LENGTH_SHORT).show();
-            return;
+            return false;
         }
 
-        //중복 재료 검사
-        for(int i = 0; i < ingredientNum; i++)
-        {
-            if(ingredientSet[i].getName().equals(s))
-            {
+        for (int i = 0; i < ingredientNum; i++) {
+            if (ingredientSet[i].getName().equals(s)) {
                 Toast.makeText(SearchActivity.this, "이미 추가된 재료입니다", Toast.LENGTH_SHORT).show();
-                return;
+                return false;
             }
         }
-
-
-        //더미 Ingredient 생성
-        final Ingredient dummy = new Ingredient(s, 0);
-
-        //동적 textview 생성
-        final TextView textview_ingredient = new TextView(SearchActivity.this);
-        textview_ingredient.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-        textview_ingredient.setBackgroundColor(Color.parseColor("#00FFFFFF"));
-
-        textview_ingredient.setTextColor(Color.parseColor("#000000"));
-        textview_ingredient.setTextSize(16);
-        textview_ingredient.setText(s);
-        textview_ingredient.setSingleLine();
-        ll.addView(textview_ingredient);
-
-        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_green);
-
-        //재료 목록 배열에 저장
-        ingredientSet[ingredientNum] = dummy;
-        ingredientNum++;
-
-        //버튼화
-        textview_ingredient.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                dummy.cycleSearchMode();
-                switch (dummy.getSearchMode())
-                {
-                    case 0:
-                        textview_ingredient.setTextColor(Color.parseColor("#000000"));
-                        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_green);
-                        break;
-                    case 1:
-                        textview_ingredient.setTextColor(Color.parseColor("#000000"));
-                        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_blue);
-                        break;
-                    case 2:
-                        textview_ingredient.setTextColor(Color.parseColor("#000000"));
-                        textview_ingredient.setBackgroundResource(R.drawable.roundingbox_red);
-                        break;
-                }
-            }
-        });
-
-
+        ingredientSet[num].setName(s);
+        return true;
     }
 }
-
