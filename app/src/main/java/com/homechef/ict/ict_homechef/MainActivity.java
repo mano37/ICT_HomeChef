e97efced1c2dbcfa44db4b056d4b9f39124aa94c
신동하 <sinwindis@naver.com>
2018-06-10 05:03:32 +0900

    메인 페이지 레시피 예시

--- app/src/main/java/com/homechef/ict/ict_homechef/MainActivity.java
+++ app/src/main/java/com/homechef/ict/ict_homechef/MainActivity.java
@@ -2,6 +2,7 @@ package com.homechef.ict.ict_homechef;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
@@ -15,13 +16,25 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.homechef.ict.ict_homechef.ConnectUtil.ConnectUtil;
+import com.homechef.ict.ict_homechef.ConnectUtil.HttpCallback;
+import com.homechef.ict.ict_homechef.ConnectUtil.ResponseBody.RecipeGet;
 import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -44,6 +57,18 @@ public class MainActivity extends AppCompatActivity
 
     TextView textView;
     ImageView imageView;
+
+    //////////////////////////동하가 추가한 변수들
+    ConnectUtil connectUtil = ConnectUtil.getInstance(this).createBaseApi();
+    Map<String,String> header = new HashMap<>();
+    static int int_scrollViewPos;
+    static int int_TextView_lines;
+
+    int loadedThumnail = 0;
+
+    int searchIndex;
+
+    //////////////////////////////////////////////
 
 
     @Override
@@ -102,13 +127,54 @@ public class MainActivity extends AppCompatActivity
                 .into(imageView);
 
 
-        textView = (TextView)findViewById(R.id.text1);
-        textView.setText(userNameStr);
-
 
         // Make SharedPreference
         pref = PreferenceManager.getDefaultSharedPreferences(this);
         editor = pref.edit();
+
+        ////////////////////////////////////////////랜덤 레시피////////////////////////////////////////////
+
+
+        System.out.println("this is Main Page Random Recipe");
+        //변수 선언
+        Intent intent = getIntent();
+        int ingredientNum;
+        final ScrollView svRecipeList = findViewById(R.id.main_sv_main);
+        final LinearLayout llRecipeList = findViewById(R.id.main_ll_recipelist);
+
+
+
+
+        makeHeader(userJson.get("jwt_token").getAsString());
+
+        for(int i = 0; i < 3; i++)
+        {
+            int id = (int) (Math.random() * (120000)) + 1;
+            recipesGet(String.valueOf(id), llRecipeList);
+        }
+
+
+        //화면 최하단 스크롤
+
+
+        //Detect Bottom ScrollView
+        svRecipeList.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+            @Override
+            public void onScrollChanged() {
+                int_scrollViewPos = svRecipeList.getScrollY();
+                int_TextView_lines = svRecipeList.getChildAt(0).getBottom() - svRecipeList.getHeight();
+
+                if(int_TextView_lines == int_scrollViewPos){
+                    //화면 최하단 스크롤시 이벤트
+                    int id = (int) (Math.random() * (120000)) + 1;
+                    recipesGet(String.valueOf(id), llRecipeList);
+
+                }
+
+            }
+        });
+        //////////////////////////////////////////////////////////////////////////////////////////////////
+
     }
 
     @Override
@@ -153,7 +219,7 @@ public class MainActivity extends AppCompatActivity
         // 레시피 검색으로 이동
         if (id == R.id.nav_searchRecipe) {
             Intent intent = new Intent(this, SearchActivity.class);
-            intent.putExtra("user_info",userInfo);
+            intent.putExtra("user_info", userInfo);
             startActivity(intent);
 
         }
@@ -201,8 +267,6 @@ public class MainActivity extends AppCompatActivity
                 userInfo = data.getStringExtra("user_info");
                 userJson = (JsonObject) parser.parse(userInfo);
                 String str = userJson.get("name").toString();
-                textView = (TextView)findViewById(R.id.text1);
-                textView.setText(str);
 
                 editor.putString(tokenKeyName, userJson.get(tokenKeyName).toString());
                 editor.commit();
@@ -212,13 +276,159 @@ public class MainActivity extends AppCompatActivity
                 userInfo = data.getStringExtra("user_info");
                 userJson = (JsonObject) parser.parse(userInfo);
                 String str = userJson.get("name").toString();
-                textView = (TextView)findViewById(R.id.text1);
-                textView.setText(str);
-
                 editor.putString(tokenKeyName, userJson.get(tokenKeyName).toString());
                 editor.commit();
             }
         }
+
+    }
+
+    ////////////////////////////////////동하가 추가한 메소드들
+    private void showRecipeThumnail(final ThumnailInfo ti, LinearLayout ll)
+    {
+        //Thumnail의 틀 레이아웃
+        final LinearLayout ll_thumnail = new LinearLayout(MainActivity.this);
+        ll_thumnail.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        ll_thumnail.setBackgroundResource(R.drawable.boxline_gradientwhite);
+        ll_thumnail.setOrientation(LinearLayout.VERTICAL);
+
+        //Thumnail 내의 이미지 틀 레이아웃
+        final LinearLayout ll_image = new LinearLayout(MainActivity.this);
+        ll_image.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        //Thumnail 내의 메인 이미지
+        ImageView image_thumnail = new ImageView(MainActivity.this);
+        image_thumnail.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+        image_thumnail.setImageResource(R.drawable.thumnail);
+        if(!ti.getImgUrl().isEmpty())
+        {
+            Picasso.get().load(ti.getImgUrl()).resize(ll.getWidth(), 0).centerCrop().into(image_thumnail);
+        }
+
+
+        final LinearLayout ll_content = new LinearLayout(MainActivity.this);
+        ll_content.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        ll_content.setOrientation(LinearLayout.VERTICAL);
+        ll_content.setPadding(10, 10, 10, 10);
+
+        final TextView tv_recipeName = new TextView(MainActivity.this);
+        tv_recipeName.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        tv_recipeName.setTextColor(Color.parseColor("#000000"));
+        tv_recipeName.setPadding(10, 10, 10, 10);
+        tv_recipeName.setTextSize(18);
+        tv_recipeName.setText(ti.getRecipeName());
+        tv_recipeName.setSingleLine();
+
+        final TextView tv_ingredientList = new TextView(MainActivity.this);
+        tv_ingredientList.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        tv_ingredientList.setTextColor(Color.parseColor("#000000"));
+        tv_ingredientList.setPadding(10, 10, 10, 10);
+        tv_ingredientList.setTextSize(12);
+        String recipeList = "재료: ";
+        for(int i = 0; i < ti.getIngredientList().size(); i++)
+        {
+            recipeList += ti.getIngredientList().get(i) + " ";
+        }
+        tv_ingredientList.setText(recipeList);
+        tv_ingredientList.setSingleLine();
+
+        final TextView tv_recommendCount = new TextView(MainActivity.this);
+        tv_recommendCount.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        tv_recommendCount.setTextColor(Color.parseColor("#000000"));
+        tv_recommendCount.setPadding(10, 10, 10, 10);
+        tv_recommendCount.setTextSize(10);
+        tv_recommendCount.setText("추천수 " + ti.getCount());
+        tv_recommendCount.setSingleLine();
+
+        final TextView tv_writerName = new TextView(MainActivity.this);
+        tv_writerName.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        tv_writerName.setTextColor(Color.parseColor("#000000"));
+        tv_writerName.setPadding(10, 10, 10, 10);
+        tv_writerName.setTextSize(12);
+        tv_writerName.setText(ti.getWriterName());
+        tv_writerName.setSingleLine();
+
+        //완성된 썸네일 레이아웃을 레이아웃에 표출
+        ll.addView(ll_thumnail);
+
+        //썸네일 레이아웃에 내용 표출
+        ll_thumnail.addView(ll_image);
+        ll_thumnail.addView(ll_content);
+
+        ll_image.addView(image_thumnail);
+
+
+        ll_content.addView(tv_recipeName);
+        ll_content.addView(tv_ingredientList);
+        ll_content.addView(tv_recommendCount);
+        ll_content.addView(tv_writerName);
+
+        ll_thumnail.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, RecipeInfoActivity.class);
+                intent.putExtra("id", ti.getRecipeId());
+                intent.putExtra("user_info", userInfo);
+                System.out.println("startActivitiy - recipeInfoActivity @@@@@@");
+                System.out.println("userInfo is : " + userInfo);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void makeHeader(String jwt_token){
+
+        String token = "Bearer " + jwt_token;
+        header.put("Authorization",token);
+
+    }
+
+    public void recipesGet(String id, final LinearLayout ll) {
+
+        connectUtil.getRecipe(header, id, new HttpCallback() {
+            @Override
+            public void onError(Throwable t) {
+                // 내부적 에러 발생할 경우
+                System.out.println("RecipeShownByMe getRecipe onError@@@@@@");
+            }
+            @Override
+            public void onSuccess(int code, Object receivedData) {
+                // 성공적으로 완료한 경우
+                System.out.println("RecipeShownByMe getRecipe onSuccess1 @@@@@@");
+                RecipeGet data = (RecipeGet) receivedData;
+                Iterator<String> ingreVal = data.ingre_count.keySet().iterator();
+                int j = 0;
+                ArrayList<String> ingreList = new ArrayList<>();
+                while(ingreVal.hasNext())
+                {
+                    String keys = (String)ingreVal.next();
+                    ingreList.add(keys);
+                }
+                ThumnailInfo thumnailinfo = new ThumnailInfo(
+                        data.recipe_id,
+                        data.title,
+                        data.image_url,
+                        ingreList,data.author_name,
+                        data.created_at,
+                        data.recommend_count);
+                showRecipeThumnail(thumnailinfo, ll);
+
+
+                System.out.println("RecipeShownByMe getRecipe onSuccess@@@@@@");
+
+            }
+
+            @Override
+            public void onFailure(int code) {
+                // 통신에 실패한 경우
+                // 결과값이 없다거나, 서버에서 오류를 리턴했거나
+                // 또는 ResponseBody 안의 key 값이 이상하거나
+                System.out.println("RecipeShownByMe getRecipe onFailure@@@@@@");
+            }
+        });
 
     }
 }
